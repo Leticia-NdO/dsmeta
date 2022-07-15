cfff761c2becb88d1ba07d323c8454f175ee2fae
Leticia-NdO <3972656@alunouninter.com>
2022-07-15 14:06:16 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { toast } from 'react-toastify'
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request'
 import './styles.css'
@@ -8,7 +9,9 @@ type Props = {
 }
 
 function notify(id: number) {
-    axios.get(`${BASE_URL}/sales/${id}/notification`)
+    axios.get(`${BASE_URL}/sales/${id}/notification`).then(() => {
+        toast.info('SMS enviado com sucesso!')
+    })
 }
 
 
